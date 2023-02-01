fa373bd5bb78b710fb7bce8f66cb112907de5b8a
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-02-01 20:49:37 +0100

    using fetch instead

--- consumer.js
+++ consumer.js
@@ -1,73 +1,76 @@
 var amqp = require('amqplib/callback_api');
 var nodemailer = require('nodemailer');
 require('dotenv').config();
-const axios = require('axios');
 
 const authServiceUrl = `${process.env.AUTHENTICATIONURL}/users/emails`;
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASSWORD
-  }
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD
+    }
 });
 const RabbitMQConfig = {
     NEW_JOB_EXCHANGE: "NEW_JOB_EXCHANGE",
     NEW_JOB_QUEUE: "NEW_JOB_QUEUE",
     NEW_JOB_ROUTING_KEY: "new.job"
-  };
-  
-  amqp.connect(`amqp://${process.env.RABBITURL}`, (error0, connection) => {
+};
+
+amqp.connect(`amqp://${process.env.RABBITURL}`, (error0, connection) => {
     if (error0) {
-      throw error0;
+        throw error0;
     }
     connection.createChannel((error1, channel) => {
-      if (error1) {
-        throw error1;
-      }
-      channel.assertExchange(RabbitMQConfig.NEW_JOB_EXCHANGE, 'topic', {
-        durable: false
-      });
-      channel.assertQueue(RabbitMQConfig.NEW_JOB_QUEUE, {
-        exclusive: false
-      }, (error2, q) => {
-        if (error2) {
-          throw error2;
+        if (error1) {
+            throw error1;
         }
-        console.log("Waiting for messages in %s", q.queue);
-        channel.bindQueue(q.queue, RabbitMQConfig.NEW_JOB_EXCHANGE, RabbitMQConfig.NEW_JOB_ROUTING_KEY);
-        channel.consume(q.queue, (message) => {
-          if (message !== null) {
-            const messageContent = message.content.toString();
-            console.log("Received %s", messageContent);
-            axios.get(authServiceUrl)
-              .then((response) => {
-                const emails = response.data.emails;
-                emails.forEach((email) => {
-                  const mailOptions = {
-                    from: process.env.EMAIL,
-                    to: email,
-                    subject: 'New job available',
-                    text: messageContent.title
-                  };
-                  transporter.sendMail(mailOptions, (error, info) => {
-                    if (error) {
-                      console.log(error);
-                    } else {
-                      console.log('Email sent: %s', info.messageId);
-                    }
-                  });
-                });
-              })
-              .catch((error) => {
-                console.error(error);
-              });
-            channel.ack(message);
-          }
-        }, {
-          noAck: false
+        channel.assertExchange(RabbitMQConfig.NEW_JOB_EXCHANGE, 'topic', {
+            durable: false
         });
-      });
+        channel.assertQueue(RabbitMQConfig.NEW_JOB_QUEUE, {
+            exclusive: false
+        }, (error2, q) => {
+            if (error2) {
+                throw error2;
+            }
+            console.log("Waiting for messages in %s", q.queue);
+            channel.bindQueue(q.queue, RabbitMQConfig.NEW_JOB_EXCHANGE, RabbitMQConfig.NEW_JOB_ROUTING_KEY);
+            channel.consume(q.queue, (message) => {
+                if (message !== null) {
+                    const messageContent = message.content.toString();
+                    console.log("Received %s", messageContent);
+                    const job = JSON.parse(messageContent);
+                    const title = job.title;
+                    fetch(authServiceUrl)
+                        .then((response) => response.json())
+                        .then((data) => {
+                            const emails = data.emails;
+                            emails.forEach((email) => {
+                                const mailOptions = {
+                                    from: process.env.EMAIL,
+                                    to: email,
+                                    subject: 'New job available',
+                                    text: title
+                                };
+                                transporter.sendMail(mailOptions, (error, info) => {
+                                    if (error) {
+                                        console.log(error);
+                                    } else {
+                                        console.log('Email sent: %s', info.messageId);
+                                    }
+                                });
+                            });
+                        })
+                        .catch((error) => {
+                            console.error(error);
+                        });
+
+                    channel.ack(message);
+                }
+            }, {
+                noAck: false
+            });
+        });
     });
-  });
+});
