e08d49bcad0064d76dd96c8a46594352c16b0b4e
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-05-05 17:11:25 +0200

    updated service

--- consumer.js
+++ consumer.js
@@ -1,6 +1,8 @@
 const {ServiceBusClient} = require('@azure/service-bus');
 const {SecretClient} = require("@azure/keyvault-secrets");
 const {DefaultAzureCredential} = require("@azure/identity");
+var nodemailer = require('nodemailer');
+
 
 require('dotenv').config();
 
@@ -8,6 +10,34 @@ const ServiceBusConfig = {
     NEW_JOB_TOPIC: 'new_job',
     NEW_JOB_SUBSCRIPTION: 'notificationservice',
 };
+
+/*var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'youremail@gmail.com',
+      pass: 'yourpassword'
+    }
+  });
+  */
+
+async function sendMail(email, text){
+    /*var mailOptions = {
+        from: 'youremail@gmail.com', //TODO: change to actual email
+        to: email,
+        subject: 'New job posted',
+        text: "New job has been posted: " + text
+      };
+   
+
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+         */
+}
 
 async function main() {
     const credential = new DefaultAzureCredential();
@@ -27,6 +57,7 @@ async function main() {
     receiver.subscribe({
             processMessage: async (brokeredMessage) => {
                 console.log(`New job: ${brokeredMessage.body}`);
+                await sendMail('email', brokeredMessage.body.text); //TODO: change to actual email - email set to temporary email for testing
                 await brokeredMessage.complete();
             },
             processError: async (args) => {
