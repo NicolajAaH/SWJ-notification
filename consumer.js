1ee75d7f91972ce2cee5a34e42b8ff18b1131caf
Nicolaj Aalykke Hansen <nicolaj.it@icloud.com>
2023-05-07 21:53:53 +0200

    added test coverage to testing

--- consumer.js
+++ consumer.js
@@ -40,18 +40,6 @@ async function sendMail(email, text){
 }
 
 async function main() {
-    const credential = new DefaultAzureCredential();
-
-    const keyVaultName = "swj-secret";
-    const url = "https://" + keyVaultName + ".vault.azure.net";
-
-    const client = new SecretClient(url, credential);
-
-    const SERVICEBUS_CONNECTION_STRING = await client.getSecret("SERVICEBUS-CONNECTION-STRING");
-
-    const serviceBusClient = new ServiceBusClient(SERVICEBUS_CONNECTION_STRING.value);
-    const receiver = serviceBusClient.createReceiver(ServiceBusConfig.NEW_JOB_TOPIC, ServiceBusConfig.NEW_JOB_SUBSCRIPTION);
-
     console.log(`Waiting for messages in ${ServiceBusConfig.NEW_JOB_SUBSCRIPTION}`);
 
     receiver.subscribe({
